c00457447a8d50b59a027b3b597fab46827f5aa7
kontastinn <badboyzzz666zzz@gmail.com>
2023-06-27 16:08:22 +0300

    Changed

--- FileCount/Program.cs
+++ FileCount/Program.cs
@@ -1,6 +1,24 @@
 using FileCount;
 
-var path = new FileExtensionCount(@"D:\C#");
 
-//path.GroupByAscending();
-//path.GroupByDescending();
+
+FileInformation objecte = new FileInformation();
+
+string path = @"D:\C#";
+
+List<FileInformation> listExtensions = new List<FileInformation>();
+
+objecte.GetDirectory(path);
+
+objecte.GetExtensions(listExtensions);
+
+objecte.OrderByDescending(listExtensions);
+
+
+foreach (var extension in listExtensions)
+{
+    Console.WriteLine(extension.GetListExtensions());
+}
+
+Console.ReadLine();
+
